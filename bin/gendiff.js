12a2fda56e62fd86003c5801951db0c2bc7a48cd
TIBET7 <lorneras@gmail.com>
2020-09-04 13:51:39 +0300

    begin step3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
+#!/usr/bin/env node
+
 import program from 'commander';
+import genDiff from '../src/genDiff.js';
 
 program
-    .version('1.0.0')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format [type]', 'output format');
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstFilePath> <secondFilePath>')
+  .option('-f, --format [type]', 'output format')
+  .action((firstFilePath, secondFilePath) => {
+    console.log(genDiff(firstFilePath, secondFilePath));
+  });
 
 program.parse(process.argv);
-
-
---
+++ src/genDiff.js
@@ -0,0 +1,59 @@
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+
+const getFileData = (filePath) => {
+  const absolutePath = path.resolve(process.cwd(), filePath);
+  const fileData = fs.readFileSync(absolutePath, 'utf-8');
+  return fileData;
+};
+
+const generateAuxiliaryData = (firstFileData, secondFileData) => {
+  const firstFileDataKeys = Object.keys(firstFileData);
+  const secondFileDataKeys = Object.keys(secondFileData);
+  const sharedKeys = _.union(firstFileDataKeys, secondFileDataKeys).sort();
+  const getDiff = sharedKeys.map((key) => {
+    if (!_.has(firstFileData, key)) {
+      return { name: key, status: 'added', value: secondFileData[key] };
+    }
+
+    if (!_.has(secondFileData, key)) {
+      return { name: key, status: 'removed', value: firstFileData[key] };
+    }
+
+    if (firstFileData[key] !== secondFileData[key]) {
+      return {
+        name: key, status: 'modified', valueRemoved: firstFileData[key], valueAdded: secondFileData[key],
+      };
+    }
+    return { name: key, status: 'unModified', value: firstFileData[key] };
+  });
+  return getDiff;
+};
+
+const getDiff = (data) => {
+  const res = data.map((item) => {
+    switch (item.status) {
+      case 'unModified':
+        return `  ${item.name}: ${item.value}`;
+      case 'removed':
+        return `- ${item.name}: ${item.value}`;
+      case 'added':
+        return `+ ${item.name}: ${item.value}`;
+      case 'modified':
+        return `- ${item.name}: ${item.valueRemoved}\n+ ${item.name}: ${item.valueAdded}`;
+      default:
+        return 'error: "wrong status property value"';
+    }
+  });
+  return `{\n${res.join('\n')}\n}`;
+};
+
+const genDiff = (firstFile, secondFile) => {
+  const firstFileData = getFileData(firstFile);
+  const secondFileData = getFileData(secondFile);
+  const auxiliaryData = generateAuxiliaryData(firstFileData, secondFileData);
+  return getDiff(auxiliaryData);
+};
+
+export default genDiff;
