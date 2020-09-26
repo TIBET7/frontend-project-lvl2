f0516382c29aeb43d39ab70835b9a8baa7f09efa
TIBET7 <lorneras@gmail.com>
2020-09-26 15:04:58 +0300

    modify format

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,31 +9,34 @@ const stringify = (item, depth = 1) => {
 };
 
 const format = (data, depth = 1) => {
-  const res = data.map((item) => {
-    const {
-      name,
-      status,
-      value,
-      child,
-      valueRemoved,
-      valueAdded,
-    } = item;
-    switch (status) {
-      case 'removed':
-        return `${getIndent(depth)}- ${name}: ${stringify(value, depth + 3)}`;
-      case 'added':
-        return `${getIndent(depth)}+ ${name}: ${stringify(value, depth + 3)}`;
-      case 'unModified':
-        return `${getIndent(depth)}  ${name}: ${stringify(value, depth + 2)}`;
-      case 'modified':
-        return `${getIndent(depth)}- ${name}: ${stringify(valueRemoved, depth + 3)}\n${getIndent(depth)}+ ${name}: ${stringify(valueAdded, depth + 3)}`;
-      case 'parentNode':
-        return `${getIndent(depth)}  ${name}: ${format(child, depth + 2)}`;
-      default:
-        return 'error: "wrong status property value';
-    }
-  });
-  return `{\n${res.join('\n')}\n${getIndent(depth - 1)}}`;
+  const iter = (innerData, depth) => {
+    const res = data.map((item) => {
+      const {
+        name,
+        status,
+        value,
+        child,
+        valueRemoved,
+        valueAdded,
+      } = item;
+      switch (status) {
+        case 'removed':
+          return `${getIndent(depth)}- ${name}: ${stringify(value, depth + 3)}`;
+        case 'added':
+          return `${getIndent(depth)}+ ${name}: ${stringify(value, depth + 3)}`;
+        case 'unModified':
+          return `${getIndent(depth)}  ${name}: ${stringify(value, depth + 2)}`;
+        case 'modified':
+          return `${getIndent(depth)}- ${name}: ${stringify(valueRemoved, depth + 3)}\n${getIndent(depth)}+ ${name}: ${stringify(valueAdded, depth + 3)}`;
+        case 'parentNode':
+          return `${getIndent(depth)}  ${name}: ${format(child, depth + 2)}`;
+        default:
+          return 'error: "wrong status property value';
+      }
+    });
+    return `{\n${res.join('\n')}\n${getIndent(depth - 1)}}`;
+  };
+  return iter(data, 1);
 };
 
 export default format;
