ce7a5183fef036e8339334c1e53a5af3c65db8cb
TIBET7 <lorneras@gmail.com>
2020-10-01 15:56:00 +0300

    make refactoring

--- src/parsers.js
+++ src/parsers.js
@@ -1,5 +1,21 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
+import _ from 'lodash';
+
+const parseNumber = (data) => {
+  const newData = JSON.parse(JSON.stringify(data));
+  const dataKeys = Object.keys(newData);
+  dataKeys.map((key) => {
+    if (_.isPlainObject(data[key])) {
+      newData[key] = parseNumber(data[key]);
+    }
+    if (parseInt(data[key], 10)) {
+      newData[key] = parseInt(data[key], 10);
+    }
+    return newData[key];
+  });
+  return newData;
+};
 
 const parseData = (fileFormat, data) => {
   switch (fileFormat) {
@@ -8,8 +24,7 @@ const parseData = (fileFormat, data) => {
     case 'yml':
       return yaml.safeLoad(data);
     case 'ini':
-      console.log(ini.parse(data));
-      return ini.parse(data);
+      return parseNumber(ini.parse(data));
     default:
       return `error: ${fileFormat} is invalid fileFormat`;
   }
